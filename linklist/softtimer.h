9936c7337f7ef3efb65c5de96bbcf8658c148488
XZP <xingzhipeng2@126.com>
2018-01-14 22:09:32 +0800

    Update softtimer.h

--- linklist/softtimer.h
+++ linklist/softtimer.h
@@ -12,10 +12,10 @@ typedef uint32 (*PF_SOFTTIMER_CB)(void *pPara);
 
 typedef struct _T_SOFTTIMER  
 {  
-    PF_SOFTTIMER_CB *pfSofttimerCB;   /* »Øµ÷º¯Êý */
+    PF_SOFTTIMER_CB pfSofttimerCB;    /* »Øµ÷º¯Êý */
     uint8  u8TaskID;                  /* ¶¨Ê±Æ÷ID */
-    uint16 u16OldTime;                /* ³õÊ¼Ê±¼ä */
-    uint16 u16Period;                 /* ¶¨Ê±Ê±¼ä */
+    uint16 u16OldTime;                /* »ù×¼Ê±¼ä */
+    uint16 u16Period;                 /* ¶¨Ê±ÖÜÆÚ */
     uint16 u16Count;                  /* Ö´ÐÐ´ÎÊý */
 }T_SOFTTIMER;
 
@@ -26,79 +26,75 @@ typedef struct _T_SOFTTIMER_NODE
     T_SOFTTIMER tSofttimer;
 }T_SOFTTIMER_NODE;
 
+extern PF_SOFTTIMER_CB g_aptSofttimerTimeout[E_SOFTTIMER_TASK_NUM];
 
 /******************************************************************************
-* Name       : unsigned char CreateLinklist(void)
-* Function   : Create a Linklist
+* Name       : uint8 SofttimerInit(void)
+* Function   : Create a Linklist head
 * Input      : None
 * Output:    : None
-* Return     : FAULT:Create list failed
-*              TRUE: Create list success
+* Return     : FAULT:Create linklist failed
+*              TRUE: Create linklist success
 * Description: None
 * Version    : V1.00
 * Author     : XZP
-* Date       : 11th Jan 2018
+* Date       : 14th Jan 2018
 ******************************************************************************/
 uint8 SofttimerInit(void);
 
 /******************************************************************************
-* Name       : T_NODE *InsertLinklist(unsigned int wData,unsigned char ucInsertPlace)
-* Function   : Insert a node into linklist
-* Input      : unsigned int wData      :The data to be inserted
-*              unsigned char ucAddPlace:  
-*                          LINKLIST_INSERT_HEAD:Inserted the node into head
-*                          LINKLIST_INSERT_TAIL:Inserted the node into tail
+* Name       : uint16 CreateSofttimer(T_SOFTTIMER *ptSofttimer)
+* Function   : Create a softtimer linklist to tail
+* Input      : T_SOFTTIMER *ptSofttimer
 * Output:    : None
-* Return     : NULL:Insert list failed
-*              pAdd:The address of insert node
+* Return     : FAULT:Create new softtimer failed
+*              TRUE: Create new softtimer success
 * Description: None
 * Version    : V1.00
 * Author     : XZP
-* Date       : 11th Jan 2018
+* Date       : 14th Jan 2018
 ******************************************************************************/
 uint16 CreateSofttimer(T_SOFTTIMER *ptSofttimer);
 
 /******************************************************************************
-* Name       : unsigned char DeleteLinklist(T_NODE *pDelAddr)
-* Function   : Delete a node from list
-* Input      : T_NODE *pDelAddr:The address of delete node
+* Name       : uint16 DeleteSofttimer(uint8 u8TaskID)
+* Function   : Delete a softtimer from list
+* Input      : uint8 u8TaskID
 * Output:    : None
-* Return     : FAULT:Delete list failed
-*              TRUE: Delete list success
+* Return     : FAULT:Delete softtimer failed
+*              TRUE: Delete softtimer success
 * Description: None
 * Version    : V1.00
 * Author     : XZP
-* Date       : 11th Jan 2018
+* Date       : 14th Jan 2018
 ******************************************************************************/
-uint16 DeleteSofttimer(uint8 ucTaskID);
+uint16 DeleteSofttimer(uint8 u8TaskID);
 
 /******************************************************************************
-* Name       : void DeleteAllLinklist(void)
-* Function   : Delete all node from list
+* Name       : void DeleteAllSofttimer(void)
+* Function   : Delete all softtimer from list
 * Input      : None
 * Output:    : None
 * Return     : None
 * Description: None
 * Version    : V1.00
 * Author     : XZP
-* Date       : 11th Jan 2018
+* Date       : 14th Jan 2018
 ******************************************************************************/
 void DeleteAllSofttimer(void);
 
 /******************************************************************************
-* Name       : void DeleteAllLinklist(void)
-* Function   : Delete all node from list
-* Input      : None
+* Name       : void ProcessSofttimer(uint16 u16CurrentTime)
+* Function   : Process softtimer
+* Input      : uint16 u16CurrentTime
 * Output:    : None
 * Return     : None
 * Description: None
 * Version    : V1.00
 * Author     : XZP
-* Date       : 11th Jan 2018
+* Date       : 14th Jan 2018
 ******************************************************************************/
-void ProcessSofttimer(uint16 uint16CurrentTime);
-
-
+void ProcessSofttimer(uint16 u16CurrentTime);
 
 #ifdef __cplusplus
 }
